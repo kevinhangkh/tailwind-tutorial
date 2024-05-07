7a8f2aa8a0f413ad51798c3285367f4d0f7ced4c
Kevin Hang <kevinhang.kh@gmail.com>
2024-05-07 13:28:02 +0200

    Setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div>
+      <section>
+        <p className="bg-green-200">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, impedit fugit saepe nobis voluptatum incidunt nam consectetur rerum, cumque, numquam laboriosam voluptates libero aperiam enim quaerat doloribus non eos vero!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </section>
     </div>
   );
 }
