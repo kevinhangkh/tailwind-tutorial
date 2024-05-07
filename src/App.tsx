fabc2f4034c1e8bd0b70a9417421f95bfa18a9f2
Kevin Hang <kevinhang.kh@gmail.com>
2024-05-07 13:40:39 +0200

    Add custom color to themes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <section>
-        <p className="bg-green-200 m-12 flex h-screen items-center justify-center text-center text-3xl">
+        <p className="bg-my-green m-12 flex h-screen items-center justify-center text-center text-3xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, impedit fugit saepe nobis voluptatum incidunt nam consectetur rerum, cumque, numquam laboriosam voluptates libero aperiam enim quaerat doloribus non eos vero!
         </p>
       </section>
